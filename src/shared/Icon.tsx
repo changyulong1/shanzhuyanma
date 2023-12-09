5b528ec2dbe53f55a4ea4338883a694b2963c9d8
long <853044263@qq.com>
2023-12-09 15:17:49 +0800

    overlay 初步完成！

--- src/shared/Icon.tsx
+++ src/shared/Icon.tsx
@@ -1,16 +1,20 @@
 import { defineComponent, PropType } from 'vue'
 import s from './Icon.module.scss'
-export type IconName = 'add' | 'chart' | 'clock' | 'cloud' | 'mangosteen' | 'pig' | 'menu'
+export type IconName = 'add' | 'chart' | 'clock' | 'cloud' |
+    'mangosteen' | 'pig' | 'menu' | 'charts' | 'notify' | 'export'
 export const Icon = defineComponent({
     props: {
         name: {
             type: String as PropType<IconName>,
             required: true,
+        },
+        onClick: {
+            type: Function as PropType<(e: MouseEvent) => void>
         }
     },
     setup(Props, context) {
         return () => (
-            <svg class={s.icon}>
+            <svg class={s.icon} onClick={Props.onClick}>
                 <use xlinkHref={'#' + Props.name}></use>
             </svg>
         )
--- src/views/StartPage.tsx
+++ src/views/StartPage.tsx
@@ -1,30 +1,37 @@
-import { defineComponent } from 'vue'
+import { defineComponent, ref } from 'vue'
 import { Button } from '../shared/Button'
 import { Center } from '../shared/Center'
 import { FloatButton } from '../shared/FloatButton'
 import { Icon } from '../shared/Icon'
 import { Navbar } from '../shared/Navbar'
+import { Overlay } from '../shared/Overlay'
 import s from './StartPages.module.scss'
 export const StartPages = defineComponent({
     setup() {
-        const go = () => {
-            console.log(666)
+        const refOverlayVisible = ref(false)
+        const onClickMenu = () => {
+            refOverlayVisible.value = !refOverlayVisible.value
         }
         return () => (
             <div>
                 <Navbar>{
                     {
                         default: () => '山竹记账',
-                        icon: () => <Icon name='menu' class={s.navIcon}></Icon>
+                        icon: () => <Icon name='menu' onClick={onClickMenu} class={s.navIcon}></Icon>
                     }
                 }</Navbar>
                 <Center class={s.pig_wrapper}>
                     <Icon name='pig' class={s.pig} />
                 </Center>
                 <div class={s.button_wrapper}>
-                    <Button onClick={go} class={s.button}>开始记账</Button>
+                    <Button class={s.button}>开始记账</Button>
                 </div>
                 <FloatButton iconName='add'></FloatButton>
+                {
+                    refOverlayVisible.value &&
+                    <Overlay onClose={() => { refOverlayVisible.value = false }} />
+                }
+
             </div>
 
         )
