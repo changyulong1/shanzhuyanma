0456bfd480c68224b016e9c73096a2bc4c29b7ec
long <853044263@qq.com>
2023-12-09 11:03:56 +0800

    center组件完成

--- src/views/StartPage.tsx
+++ src/views/StartPage.tsx
@@ -1,6 +1,8 @@
 import { defineComponent } from 'vue'
 import { Button } from '../shared/Button'
+import { Center } from '../shared/Center'
 import { FloatButton } from '../shared/FloatButton'
+import { Icon } from '../shared/Icon'
 import s from './StartPages.module.scss'
 export const StartPages = defineComponent({
     setup() {
@@ -9,6 +11,10 @@ export const StartPages = defineComponent({
         }
         return () => (
             <div>
+                <main>nav</main>
+                <Center class={s.pig_wrapper}>
+                    <Icon name='pig' class={s.pig} />
+                </Center>
                 <div class={s.button_wrapper}>
                     <Button onClick={go} class={s.button}>开始记账</Button>
                 </div>
