3aa591121e0226cbaac9ef68931fff6fc9090476
long <853044263@qq.com>
2024-06-26 22:38:01 +0800

    进度条图标bug修改

--- src/shared/Bars.tsx
+++ src/shared/Bars.tsx
@@ -22,32 +22,28 @@ export const Bars = defineComponent({
         })
         return () => (
             <div class={s.wrapper}>
-                {betterData3.value.map(({ tag, amount, percent }) => {
-                    return (
-                        <div class={s.topItem}>
-                            {(props.data && props.data.length > 0) ?
-                                props.data.map(({ tag, amount, percent }) => {
-                                    return (
-                                        <div class={s.topItem}>
-                                            <div class={s.sign}>
-                                                {tag.sign}
-                                            </div>
-                                            <div class={s.bar_wrapper}>
-                                                <div class={s.bar_text}>
-                                                    <span> {tag.name} - {percent}% </span>
-                                                    <span> ￥<Money value={amount} /> </span>
-                                                </div>
-                                                <div class={s.bar}>
-                                                    <div class={s.bar_inner} style={{ width: `${percent}%` }}></div>
-                                                </div>
-                                            </div>
+                {(props.data && props.data.length > 0) ?
+                    props.data.map(({ tag, amount, percent }) => {
+                        return (
+                            <div class={s.topItem}>
+                                <div class={s.topItem}>
+                                    <div class={s.sign}>
+                                        {tag.sign}
+                                    </div>
+                                    <div class={s.bar_wrapper}>
+                                        <div class={s.bar_text}>
+                                            <span> {tag.name} - {percent}% </span>
+                                            <span> ￥<Money value={amount} /> </span>
                                         </div>
-                                    )
-                                }) : <div>没有数据</div>
-                            }
-                        </div>
-                    )
-                })}
+                                        <div class={s.bar}>
+                                            <div class={s.bar_inner} style={{ width: `${percent}%` }}></div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        )
+                    }) : <div>没有数据</div>
+                }
             </div>
         )
     }
