73a41f433d60ba1ebecaf75dd87c2090865c6f31
long <853044263@qq.com>
2024-07-01 20:58:45 +0800

    解决：请求失败导致出现的报错

--- src/hooks/useAfterMe.ts
+++ src/hooks/useAfterMe.ts
@@ -4,7 +4,13 @@ import { useMeStore } from "../stores/useMeStores"
 export const useAfterMe = (fn: () => void) => {
     const meStore = useMeStore()
     onMounted(async () => {
-        await meStore.mePromise
-        fn()
+        //方法一：使用try catch 请求失败导致出现的报错
+        // try {
+        //     await meStore.mePromise
+        // } catch (error) {
+        //     return
+        // }
+        //方法二：请求失败导致出现的报错
+        await meStore.mePromise?.then(fn, () => undefined)
     })
 }
